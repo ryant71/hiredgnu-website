7bb68e0c7f1f4935fcea4f512fc298f9fb39c425
Ryan Tracey <91012+ryant71@users.noreply.github.com>
2025-04-23 09:44:51 +0200

    Update test-waf.sh

--- infrastructure/tools/test-waf.sh
+++ infrastructure/tools/test-waf.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+[[ -z "$1" ]] && { echo "Usage: $0 <URL>"; exit; } || url=$1
+
 for i in {1..3000}; do
-  curl -s -o /dev/null https://hiredgnu.net/ & 
+  curl -s -o /dev/null "$url" & 
 done
 wait
 
